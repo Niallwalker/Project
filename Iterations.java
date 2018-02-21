59ad9082b929c21ace5e2d15c2125c84bda4ff2f
Theferrariguy5 <36442988+Theferrariguy5@users.noreply.github.com>
2018-02-21 13:58:22 +0000

    Add files via upload

---
+++ Iterations.java
@@ -0,0 +1,14 @@
+public class Iterations{
+	public static void main(String[] args){
+	for(i = 1; i <= 5; i++){
+		for(j = 1; i <= 3; j++){
+			for(k = 1; k <= 4; k++){
+				System.out.print('*');
+
+			System.out.println();
+		}
+
+		System.out.println();
+		}
+	}
+}
